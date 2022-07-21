d2907beb3dfa25d43bad214e5de26570a29304c1
agbofolly <follyagboteko@gmail.com>
2022-07-21 14:10:04 -0700

    Adjustments

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from btre_project.listings.views import listing
 # Create your views here.
 from listings.models import Listing
 
 def  index(request):
-    listings = Listing.order_by('-listing_date').filter(is_published=True)[:3]
-    return render(request,'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request,'pages/index.html', context)
 
 def about(request):
     return render(request,'pages/about.html')
